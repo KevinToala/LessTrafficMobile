7def8d1a039656adc908bb8736a3bfd687621e0c
Kevin Toala <kevin.toala@outlook.com>
2018-04-09 21:06:02 -0500

    Integracion con Google Maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AgmCoreModule } from '@agm/core';
+
+import { Geolocation } from '@ionic-native/geolocation';
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +19,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyADH3m-qI1asuTPqk6kIM-VJSmJfVrOZuc'
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +32,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Geolocation
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Geolocation, Geoposition} from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
+    geolocation.watchPosition().subscribe((position: Geoposition) => {
+      this.lat = position.coords.latitude;
+      this.lng = position.coords.longitude;
+    });
   }
 
 }
