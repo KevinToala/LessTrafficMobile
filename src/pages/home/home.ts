c67e2428fc0d71ebbcdca2cacdf74c3ae66b72f0
Kevin Toala <kevin.toala@outlook.com>
2018-04-10 19:58:53 -0500

    Obtencion de posicion mas precisa

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,32 +9,39 @@ import {Geolocalization} from "../../app/geolocalization";
   templateUrl: 'home.html'
 })
 export class HomePage {
-  lat: number = 51.678418;
+  lat: number = 1.678418;
   lng: number = 7.809007;
 
   constructor(public navCtrl: NavController,
               private geolocation: Geolocation,
               private httpClient: HttpClient,
               private toastCtrl: ToastController) {
-    geolocation.watchPosition().subscribe((position: Geoposition) => {
-      this.lat = position.coords.latitude;
-      this.lng = position.coords.longitude;
 
-      const geolocalization: Geolocalization = {
-        latitude: this.lat,
-        longitude: this.lng
-      };
-
-      this.httpClient.post('http://localhost:8080/nodes', geolocalization).subscribe(data => {
-        let message = "Nodo insertado" + JSON.stringify(data);
-        this.toastCtrl.create({
-          message: message,
-          duration: 1000
-        }).present();
-
-        console.log(message);
-      });
-    });
   }
 
+  ionViewDidEnter(){
+    this.geolocation.watchPosition({enableHighAccuracy: true}).subscribe((position: Geoposition) => {
+      let coords = position.coords;
+
+      if(coords){
+        this.lat = coords.latitude;
+        this.lng = coords.longitude;
+
+        const geolocalization: Geolocalization = {
+          latitude: this.lat,
+          longitude: this.lng
+        };
+
+        this.httpClient.post('http://localhost:8080/nodes', geolocalization).subscribe(data => {
+          let message = "Nodo insertado" + JSON.stringify(data);
+          this.toastCtrl.create({
+            message: message,
+            duration: 1000
+          }).present();
+
+          console.log(message);
+        });
+      }
+    });
+  }
 }
